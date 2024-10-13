56d693b87c4f60561b691ec2e263209d61bd1224
gutslfo <pierretran.work@gmail.com>
2024-10-14 01:34:49 +0200

    strlcat strlcpy

--- libft.a/ft_strlcat.c
+++ libft.a/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pitran <pitran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:38:55 by pitran            #+#    #+#             */
-/*   Updated: 2024/10/13 20:36:25 by pitran           ###   ########.fr       */
+/*   Updated: 2024/10/14 01:33:08 by pitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-	size_t	j;
+	size_t	dstlen; 
+	size_t	srclen;
+	size_t	k;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dstsize <= dst_len)
-		return (dstsize + src_len);
-	i = dst_len;
-	j = 0;
-	while (src[i] && i + j < (dstsize - 1))
+	dstlen = ft_strlen(dst); /* longueur de dst */
+	srclen = ft_strlen(src); /* longueur de src */
+	if (dstsize <= dstlen) /* la taille du buffer est inférieure ou égale à la taille de dst : on retourne la taille  minimale nécessaire pour concaténer */
+		return (dstsize + srclen);
+	k = 0;
+	while (src[k] && (dstlen + k < (dstsize - 1))) /* tant que src n'est pas terminée et qu'il reste de la place pour le faire */
 	{
-		dst[i] = src[j];
-		j++;
+		dst[dstlen + k] = src[k]; /* concaténation */
+		k++;
 	}
-	dst[i + j] = '\0';
-	return (ft_strlen(dst));
+	dst[dstlen + k] = '\0'; /* on termine la string */
+	return (dstlen + srclen);
 }
--- libft.a/ft_strlcpy.c
+++ libft.a/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pitran <pitran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 10:42:31 by pitran            #+#    #+#             */
-/*   Updated: 2024/10/07 14:34:50 by pitran           ###   ########.fr       */
+/*   Updated: 2024/10/14 01:17:03 by pitran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	len;
+	size_t	cpylen; /* compte le nombre de caractères à copier */
 
 	len = ft_strlen(src);
-	if (len + 1 < dstsize)
-		ft_memcpy(dst, src, len + 1);
-	else if (dstsize != 0)
-		ft_memcpy(dst, src, dstsize - 1);
+	if (dstsize == 0) /* cas particulier: pas de place pour la copie */
+		return (len);
+	if (len < dstsize) /* si la longueur de src est inférieure à la taille du buffer, on copie l'intégralité de src */
+		cpylen = len;
+	else 
+		cpylen = dstsize - 1; /* sinon, on copie une partie des caractères en laissant un espace de mémoire pour '\0' */
+	ft_memcpy(dst, src, cpylen);
+	dst[cpylen] = '\0';
 	return (len);
 }
 
